2fbbf1926c524d5a0538eedc36cc8f80ff24ac09
FinexL <chickencurry972@gmail.com>
2025-07-09 17:11:38 +0800

    Initialize server with package.json and basic dependencies; add placeholder files for server and button components.

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -4,10 +4,8 @@ import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import ManagementTable from "tables/ManagementTable";
-mv;
-
-function TabPanel(npprops) {
+import ManagementTable from "./Tables/ManagementTable";
+function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
